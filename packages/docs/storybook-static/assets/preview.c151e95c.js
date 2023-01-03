f97753c12d70d193a9b536c68c589044143bf28a
Gondims <leticiagondim1.0@gmail.com>
2023-01-03 09:27:03 -0400

    ..

---
+++ packages/docs/storybook-static/assets/preview.c151e95c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.a725dee3.js";import"./iframe.de78ed28.js";import"./es.map.constructor.df6742c3.js";import"./es.number.to-fixed.d9eecb61.js";import"./index.8578b45d.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.c151e95c.js.map
