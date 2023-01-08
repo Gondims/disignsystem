4a5efbf5a373015158438b9640b21263e70ab5fd
Gondims <leticiagondim1.0@gmail.com>
2023-01-08 11:25:45 -0400

    Avatar

--- packages/docs/storybook-static/assets/es.map.constructor.df6742c3.js
+++
@@ -1,2 +0,0 @@
-var r=Object.defineProperty;var o=(n,t)=>r(n,"name",{value:t,configurable:!0});import{k as e,n as c}from"./iframe.de78ed28.js";var i=e,l=c;i("Map",function(n){return o(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},l);
-//# sourceMappingURL=es.map.constructor.df6742c3.js.map
